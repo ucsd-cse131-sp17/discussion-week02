2dc378b295f97b68bb61aa408aa6cfa0e55d937e
Rami Gökhan Kıcı <gokhankici@gmail.com>
2017-04-11 14:36:18 -0700

    generic printer

--- test.ml
+++ test.ml
@@ -2,28 +2,21 @@ open OUnit2
 open Week02
 open List
 
+let id x = x
+
 let t_string name value expected = name>::
-  (fun ctxt -> assert_equal expected value ~printer:(fun x -> x))
+  (fun ctxt -> assert_equal expected value ~printer:id)
 
 let t_int name value expected = name>::
   (fun ctxt -> assert_equal expected value ~printer:string_of_int)
 
-let string_of_int_list (l: int list) : string = 
-  let rec helper2 (l: int list) : string =
-  match l with
-  | []  -> ""
-  | h::t -> ", " ^ (string_of_int h) ^ (helper2 t)
-  in
-  let rec helper (l: int list) : string = 
-  match l with
-  | []  -> ""
-  | [h] -> string_of_int h
-  | h::t -> (string_of_int h) ^ (helper2 t)
-  in "[" ^ (helper l) ^ "]"
-;; 
+let string_of_a_list (printer: 'a -> string) (l: 'a list) : string = 
+  let ss    = List.map printer l in
+  let inner = String.concat ", " ss in
+  "[" ^ inner ^ "]"
 
 let t_int_list name value expected = name>::
-  (fun ctxt -> assert_equal expected value ~printer:string_of_int_list)
+  (fun ctxt -> assert_equal expected value ~printer:(string_of_a_list string_of_int))
 
 let suite =
 "suite">:::
